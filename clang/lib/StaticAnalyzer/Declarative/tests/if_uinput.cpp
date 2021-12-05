645a6772641fbc553a32fdb9503406b06afdef98
Andy Luo <andy-luo@uiowa.edu>
2021-12-05 13:13:09 -0600

    Userinput Testcases

---
+++ clang/lib/StaticAnalyzer/Declarative/tests/if_uinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int a;
+    std::cin >> a;
+
+    int b = 0;
+
+    if(a%2 == 1)
+        b = a + 1;
+
+    b;
+    return 1;
+}
---
+++ clang/lib/StaticAnalyzer/Declarative/tests/unrelated_uinput.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int a = 0; 
+    std::cin >> a; //I think that cin>> should act as an update(a)
+    a;
+    return 0;
+}
